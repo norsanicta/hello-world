056d7e01d32d86b16d07bd004a6e5b7b3a69efed
norsanicta <48062798+norsanicta@users.noreply.github.com>
2019-02-28 16:55:43 +0000

    Hello world into multiplication table

--- first_code.c
+++ first_code.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+ int main()
+ {
+   int x,y;
+   int table_max = 100;
+   int number_max = 20;
 
-int main() {
-  print("Hello world!");
-  return 0;
-}
+   for(x = 1; x <=table_max; x++)
+   {
+     for(y = 0; y <=number_max; y++)
+     {
+       printf("%d x %d = %d\n", x, y, x*y);
+     }
+     printf("\n");
+   }
+   return 0;
+ }
